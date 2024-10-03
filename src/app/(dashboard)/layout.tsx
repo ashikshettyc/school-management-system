440d7eb971e54a9fcb3c0f7a2a369e581769cebd
ashik <ashikshettyc@gmail.com>
2024-10-03 17:19:26 +0530

    lists

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -21,11 +21,11 @@ export default function DashboardLayout({
       <div className="w-[14%] md:w-[16%] lg:w-[16%] xl:w-[14%]">
         <Link href="/" className="flex items-center justify-center">
           <Image src="/logo.png" alt="logo" width={32} height={32} />
-          <span className="hidden lg:block">AshikSchool</span>
+          <span className="hidden lg:block font-bold">AshikSchool</span>
         </Link>
         <Menu />
       </div>
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] overflow-scroll">
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
         <Navbar />
         {children}
       </div>
---
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -0,0 +1,104 @@
+import Pagination from "@/components/Pagination";
+import Table from "@/components/Table";
+import TableSearch from "@/components/TableSearch";
+import { parentsData, role, studentsData } from "@/lib/data";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+type Parents = {
+  id: number;
+  name: string;
+  email?: string;
+  students: string[];
+  phone: string;
+  address: string;
+};
+
+const columns = [
+  {
+    header: "Info",
+    accessor: "info",
+  },
+  {
+    header: "Student Names",
+    accessor: "students",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Phone",
+    accessor: "phone",
+    className: "hidden md:table-cell",
+  },
+    {
+    header: "Address",
+    accessor: "address",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Action",
+    accessor: "action",
+  },
+];
+function ParentsListpage() {
+  const renderRow = (item: Parents) => (
+    <tr key={item.id}>
+      <td className="flex items-center gap-4 p-4">
+        <div className="flex flex-col">
+          <h3 className="font-semibold">{item.name}</h3>
+          <p className="text-xs text-gray-500">{item?.email}</p>
+        </div>
+      </td>
+      <td className="hidden md:table-cell">{item.students.join(",")}</td>
+      <td className="hidden md:table-cell">{item.phone}</td>
+      <td className="hidden md:table-cell">{item.address}</td>
+      <td>
+        <div className="flex items-center gap-2">
+          <Link href={`/list/teachers/${item.id}`}>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
+              <Image src="/edit.png" width={16} height={16} alt="" />
+            </button>
+          </Link>
+          {role === "admin" && (
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
+              <Image src="/delete.png" width={16} height={16} alt="" />
+            </button>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+  return (
+    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+      {/* top */}
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">All Parents</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSearch />
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/filter.png" alt="" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/sort.png" alt="" width={14} height={14} />
+            </button>
+            {
+            role === "admin" &&
+            (
+              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/plus.png" alt="" width={14} height={14} />
+            </button>
+            )
+           }
+          </div>
+        </div>
+      </div>
+      {/* list */}
+      <Table columns={columns} renderRow={renderRow} data={parentsData} />
+      {/* pagination */}
+      <Pagination />
+    </div>
+  );
+}
+
+export default ParentsListpage;
